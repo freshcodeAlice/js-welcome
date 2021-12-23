921bb3f0c5c2ab6ebf5938c8cf3d91f535644ed3
Alice <freshjs.ofte@gmail.com>
2021-12-23 19:45:14 +0200

    Declaration variables

---
+++ index.js
@@ -0,0 +1,21 @@
+let box; //Объявление переменной (Декларация)
+box = 3; //Инициализация переменной
+
+console.log(box);
+
+let value = 5; //Объявление и инициализация
+
+//let _ - loadash
+//let $ - jQuery
+
+//let 4u - Запрещено!
+
+
+
+/*
+console.log(1);
+console.log('hello');
+console.log(true);
+console.log(undefined);
+console.log(null);
+*/
