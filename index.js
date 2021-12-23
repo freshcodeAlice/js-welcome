e7b8f3d7737a74d538d54fd2b2a8603c0ecf25d9
Alice <freshjs.ofte@gmail.com>
2021-12-23 19:55:07 +0200

    Add notice

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let box; //Объявление переменной (Декларация)
 box = 3; //Инициализация переменной
 
+//console.log(newVariable); //Код работает до первой ошибки
+
 console.log(box);
 
 let value = 5; //Объявление и инициализация
@@ -9,4 +11,6 @@ value = 10;
 
 const box2 = 35;
 console.log(box2);
+
+let superPuperImportantVariable; // Camel Case notation
 
