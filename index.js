9fa5a4d277d070120443049541f8e2454b9018c5
Alice <freshjs.ofte@gmail.com>
2021-12-28 18:59:33 +0200

    SumTwo interface

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-// Task 1
-const a = 10;
-const b = 20;
-console.log(a*b);
+const inputValue1 = Number(prompt('Type first number'));
+const inputValue2 = Number(prompt('Type second number'));
 
-// Task 2
-const c = 15;
-const d = 5;
-console.log(c / d);
+let result;
 
-// Task 3
-const e = 5;
-const f = 30;
-console.log(e+f);
+if (isNaN(inputValue1) && isNaN(inputValue2)) {
+    result = 'Wrong';
+} else {
+    result = inputValue1 + inputValue2;
+}
 
-// Task 4
-const number = 33;
-const bool = false;
-const str = 'javascript';
-const strNum = '100';
-console.log(number, bool, str, strNum);
 
-// Task 5
-let num = 1;
-num++; // вместо num = num + 1
-num--;
-num += 11; // аналогично num = num + 11
-num -= 11;
-num *= 11;
-num /= 11;
+alert(result);
