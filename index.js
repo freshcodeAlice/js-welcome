abeae45bbf56c3f2f69e73c196aa71f9bfc23a7a
Alice <freshjs.ofte@gmail.com>
2021-12-28 18:35:29 +0200

    Fix

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-// alert - модалка с сообщением
-// prompt - модалка с инпутом, куда можно ввести сообщение
-// confirm - модалка с подтверждением или отменой
-let number;
-let result;
-let action;
+// Task 1
+const a = 10;
+const b = 20;
+console.log(a*b);
 
-number = prompt('Type the number');
+// Task 2
+const c = 15;
+const d = 5;
+console.log(c / d);
 
-action = prompt('Choose your action');
+// Task 3
+const e = 5;
+const f = 30;
+console.log(e+f);
 
-if (action === '+') {
-result = Number(number) + 2;
-} else {
-result = Number(number) - 2;    
-}
+// Task 4
+const number = 33;
+const bool = false;
+const str = 'javascript';
+const strNum = '100';
+console.log(number, bool, str, strNum);
 
-alert(result);
-
-// console.log(result);
-
-
+// Task 5
+let num = 1;
+num++; // вместо num = num + 1
+num--;
+num += 11; // аналогично num = num + 11
+num -= 11;
+num *= 11;
+num /= 11;
