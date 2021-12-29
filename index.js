ba4ca2405035d839af532cc925a611d47f458bab
Alice <freshjs.ofte@gmail.com>
2021-12-29 20:03:44 +0200

    Switch-case

--- index.js
+++ index.js
@@ -48,22 +48,42 @@ if (условие) {
 */
 
 
-const number = +prompt('Type your number');
+const userInput = prompt('Выберите позицию меню \n 1 - Чай \n 2 - Кофе \n 3 - Сок \n 4 - Лимонад \n 5 - Вода');
 
 debugger;
 
-if(isNaN(number)) {
-    console.log('Некорректное число');
-} else if (number % 5 === 0) {
-    console.log('Делится на 5');
-} else if (number % 3 === 0) {
-    console.log('Делится на 3');
-} else if (number % 2 === 0) {
-    console.log('Делится на 2');
-} else {
-    console.log('Не делится ни на 5, ни на 3, ни на 2');
+/* Конструкция switch-case
+switch (значение) {
+    case вариант:
+        что делать, если вариант подходит
+    case вариант2:
+        что делать, если вариант подходит
+    ...
+    default:
+        что делать, если ни один вариант не подошел
 }
 
+break - остановка
 
+*/
 
-console.log('end of script');
+switch (userInput) {
+    case '1':
+        console.log('Вот ваш чай');
+        break;
+    case '2':
+        console.log('Вот ваш кофе');
+        break;
+    case '3':
+        console.log('Вот ваш сок');
+        break;
+    case '4':
+        console.log('Вот ваш лимонад');
+        break;
+    case '5':
+        console.log('Вот ваша вода'); 
+        break;
+    default:
+        console.log('Такой позиции меню у нас нет');                         
+}
+
