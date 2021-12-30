655990bef311a57413baf9ab01abe07b91fc348e
Alice <freshjs.ofte@gmail.com>
2021-12-30 20:38:45 +0200

    While and Do-while

--- index.js
+++ index.js
@@ -1,86 +1,24 @@
-'use strict';
-
-/* 
-Function Declaration - можно вызвать с любой точки кода
-Function Expression - можно вызвать только после ее объявления
+const GOOD_PASSWORD = 'qwerty123';
+/* Задача: спрашивать у пользователя пароль (с помощью промпта), пока он не введет правильный
 */
 
-//const number1 = +prompt('Введите первое число');
-//const number2 = +prompt('Введите второе число');
-//const mode = prompt('Выберите действие: \n + - Сложение \n - - Вычитание \n * - Умножение \n / - деление');
+let count = 0;
+const MAX_TRY = 5;
 
-//const res = calculate(number1, number2, mode);
-//console.log(res);
+// каждый круг цикла называется итерация
 
-function square(x) {
-const result = mult(x, x);
-return result;
-}
-
-
-
-
-function sum (a, b) {
-    return a + b;
-}
-
-function sub (a, b){
-    return a - b;
-}
-
-function mult (a, b) {
-    return a * b;
-}
-
-function div (a, b){
-    return a / b;
-}
-
-// Callback - функция, переданная как аргумент другой функции
-// Функция высшего порядка - функция, принимающая на вход другие функции или возвращающая функции в результате исполнения
-
-function highOrderFunction(num1, num2, functionToPerform){
-    return functionToPerform(num1, num2);
-}
-
-
-const res = highOrderFunction(3, 4, sum);
-console.log(res);
-
-
-
-// function calculate(num1, num2, userMode) {
-//     let f;
-//     switch (userMode) {
-//         case '+':
-//             f = sum;
-//             break;
-//         case '-':
-//             f = sub;
-//             break;
-//         case '*':
-//             f = mult;
-//             break;
-//         case '/':
-//             f = div;
-//             break;               
-//     }
-
-//     if (typeof f === 'function') {
-//         const result = f(num1, num2);
-//         return result;
-//     } else {
-//         return null;
-//     }
-// }
-
-
-
-//  function greeting(name) {
-//  const checkedName = name ? name : 'Аноним';
-//  console.log('Привет, ' + checkedName);
+//  while(count++ < MAX_TRY) {
+// const userInput = prompt('Type the password');
+//    if (userInput === GOOD_PASSWORD) {
+//     break;
+//    }
 //  }
 
- 
-//  const userName = prompt('What`s your name?');
-//  greeting(userName);
+
+ do {
+ console.log('Do' + count)
+ } while (count++ < 5)
+
+
+
+ console.log('end of script');
