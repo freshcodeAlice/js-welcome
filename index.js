3d667f89264f8edf29bf0a34b8a42e491fc5532d
Alice <freshjs.ofte@gmail.com>
2022-01-04 18:35:54 +0200

    Sum of Odd task resolve

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-const GOOD_PASSWORD = 'qwerty123';
-/* Задача: спрашивать у пользователя пароль (с помощью промпта), пока он не введет правильный
-*/
 
+function getSumOfOdd (from, to) {
 
-const MAX_TRY = 5;
+    let sum = 0;
 
-// каждый круг цикла называется итерация
-
-//  while(count++ < MAX_TRY) {
-// const userInput = prompt('Type the password');
-//    if (userInput === GOOD_PASSWORD) {
-//     break;
-//    }
-//  }
-
-//  for (Инициализация;Условие;Изменение на каждом шаге итерации) {
-// блок кода
-//  }
-
-
-/* Написать функцию, которая возвращает факториал числа */
-
-function factorial(a) {
-
-    let result = 1;
-
-    for(let i = 1; i <= a; i++){
-    result *= i;
+    for (let i = from; i <= to; i++) {
+        if(i % 2 === 0) {
+            sum += i; // sum = sum + i
+        }
     }
-
-     return result
+    return sum;
 }
 
- const res = factorial(4);
- console.log(res);
+
+
+const res = getSumOfOdd(4,10);
+console.log(res);
+
+
+
+
+
+
+
+
+
+
 
 
 
