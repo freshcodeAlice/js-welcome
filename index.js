15458acfe489f600571141caee1a6ec2c6fc3de8
Alice <freshjs.ofte@gmail.com>
2022-01-04 19:08:41 +0200

    Power task resolve

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 
-/* Написать функцию, которая возвращает факториал числа */
+/* Написать функцию, которая возводит число в заданную степень */
 
-function factorial(number) {
-const MAX_TRY_FACTORIAL = 250;
-
-    if (isNaN(number)) {
-        return false;
+function getPower(base, power) {
+    if(power === 0) {
+        return 1
     }
 
-    if(number === 1 || number === 0) {
-        return 1;
+    if(power === 1) {
+        return base
     }
 
-    if(number > MAX_TRY_FACTORIAL){
-        return false;
+    let result = 1;
+
+    for (let i = 1; i <= power; i++){
+        result *= base;
     }
+    return result;
 
-    let result = 1n;
-
-    for(let i = 1; i <= number; i++){
-    result *= BigInt(i);
-    }
-
-     return result
 }
-
-
-
- // 1*2*3*4*5...*n
-
 
 
 
