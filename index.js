e80795e7103e4088d102287efc95a6dc2a9a8520
Alice <freshjs.ofte@gmail.com>
2022-01-05 20:09:30 +0200

    Key in object task

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const user = {
-    'favorite color': 'green',
-    0: 'test'
+const weekPlan = {
+    Mon: 'Visit to stomatologist',
+    Tue: 'Got salary',
+    Wed: 'Meet friends',
+    Thu: 'Hard working',
+    Fri: 'Free day',
+    Sat: 'Go to party'
 }
 
-user.favoriteColor // Доступ к свойству
 
-user['favorite color'] //Доступ к свойству с возможностью вычисления
-user[0]
-user['0']
+function getPlanning() {
+    const dayWeek = prompt('Type the day: \nMon - for monday \nTue - thuesday\n Wed - Wednesday \n Thu - Thuesday \n Fri - Friday \n Sat - Saturday');
 
-const test = 'favorite color';
+//    alert(weekPlan[dayWeek] ? weekPlan[dayWeek] : 'Nothing planned for this day');
 
-user[test]
+console.log(dayWeek in weekPlan ? weekPlan[dayWeek] : 'There is no such day in our plan'); // 'key' in object
+
+}
+
+getPlanning();
+
