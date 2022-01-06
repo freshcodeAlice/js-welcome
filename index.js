19cb4f4ddee31cda1f5e31abf8bbf7504a13ee3f
Alice <freshjs.ofte@gmail.com>
2022-01-06 18:21:53 +0200

    This issue

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict'
 
+const obj = {};
 
-const t1 = 1;
-const t2 = 1;
 
-const obj1 = {
-   test: 1,
-};
 
-const obj2 = {
-  test: 2
-};
+function MyObj() {
+//    if(!new.target) {
+//        return new MyObj()
+//    }
 
-const link = obj1; // link === obj1 ссылка на ту же область памяти
+//this - контекст
 
-link = {} //выдаст ошибку, потому что link - const. Константы запоминают ссылки на объекты!
+this.prop = 'value';
+
+}
+
+const obj2 = new MyObj();
