ad709293f1ef2d6dacbb786cd06b4d00f8c7c755
Alice <freshjs.ofte@gmail.com>
2022-01-06 18:09:07 +0200

    Const object

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 'use strict'
 
-function getMultyplyTable() {
-    const table = {};
 
-    for (let i = 1; i < 10; i++) {
-        for (let j = 1; j < 10; j++) {
-            table[`${i} * ${j}`] = i * j;
-        }
-    }
+const t1 = 1;
+const t2 = 1;
 
+const obj1 = {
+   test: 1,
+};
 
-    return table;
-}
+const obj2 = {
+  test: 2
+};
 
+const link = obj1; // link === obj1 ссылка на ту же область памяти
 
-//getMultyplyTable();
-
-/* Напишите функцию, которая принимает число и выводит все его делители от 1 до 10*/
-
-function getDividers(number) {
-const dividerTable = {};
-
-    for (let i = 1; i < 10; i++) {
-        if (number % i === 0) {
-           dividerTable[i] = number / i;
-        }
-    }
-    return dividerTable;
-}
-
-//getDividers(12);
+link = {} //выдаст ошибку, потому что link - const. Константы запоминают ссылки на объекты!
