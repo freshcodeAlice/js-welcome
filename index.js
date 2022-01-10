846f51b89cd65b1bc6fb4c491029eba834ff6bfd
Alice <freshjs.ofte@gmail.com>
2022-01-10 19:14:47 +0200

    Literal prototyping (__proto__)

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-'use strict'
+'use strict';
 
+const driver1 = {
+    name: 'First',
+};
 
-/*
-Написать функцию, принимающую в качестве параметра размер будущего квадрата и символ, которым мы будем его отрисовывать.
-Функция должна выводить (рисовать) в консоль квадрат заданной размерности с отрисованной диагональю (пример в приложенной картинке).
- */
-
-function drowSquareWithDiagonal (dimension, drowSymbol) {
-
-    let str = '';
-
-    for (let i = 0; i < dimension; i++) {
-       for (let j = 0; j < dimension; j++) {   
-        if(i === 0 || i === (dimension-1) || j === 0 || j === (dimension-1) || i === j) {
-            str+= drowSymbol;
-        } else {
-            str += ' ';
-        }
-       }
-       str += '\n';
-
-    }
-
-return str;
-
+const driver2 = {
+    name: 'Second',
 }
 
+const car = {
+    go: function() {
+        return `${this.name} is on the car`;
+    }
+}
 
-console.log(drowSquareWithDiagonal(10, '*'));
+driver1.__proto__ = car;
+driver2.__proto__ = car;
