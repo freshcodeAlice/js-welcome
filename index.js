d6d1445da7ef760d21f5b72a0932ac950e15ab91
Alice <freshjs.ofte@gmail.com>
2022-01-10 19:56:43 +0200

    Ladder Task

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 'use strict';
 
-const driver1 = {
-    name: 'First',
-};
+/* Напишите конструктор для объекта "Лестница".
+У нее должно быть:
+value - значение ступеньки, на которой вы находитесь
 
-const driver2 = {
-    name: 'Second',
+И три метода:
+up() - подняться на ступеньку вверх
+down() - спустить на ступеньку вниз
+showStep() - посмотреть текущую ступеньку
+*/
+
+/* Task2:
+Измените код таким образом, чтобы мы могли делать цепочку вызовов:
+ladder.up().up().down().up().showStep();
+*/
+
+
+function Ladder () {
+    this.value = 0;
+    this.up = function() {
+        this.value++;
+        return this;
+    };
+    this.down = function() {
+        this.value--;
+        return this;
+    };
+    this.showStep = function() {
+        return this.value;
+    };
 }
 
-const car = {
-    go: function() {
-        return `${this.name} is on the car`;
-    }
-}
-
-driver1.__proto__ = car;
-driver2.__proto__ = car;
+const ladder = new Ladder();
