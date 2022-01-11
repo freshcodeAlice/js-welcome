15626ffd14068b34ba9f993f8ae472d852bcd6b2
Alice <freshjs.ofte@gmail.com>
2022-01-11 18:45:20 +0200

    Random Array task resolve

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 'use strict';
 /* 
-Функция, которая решает квадратные уравнения
+Создать массив и заполнить его 10тью случайными числами
 */
 
-function getRootOfSquare (a, b, c) {
+function getRandomArray(dimension) {
+const arr = [];
 
-    let D = b * b - 4 * a * c;
+for (let i = 0; i < dimension; i++) {
+     arr.push(Math.round(Math.random()*10));
+}
 
-    const result = [];
-
-    if ( D < 0) {
-        return false;
-    } else if ( D === 0) {
-        result[0] = (b / 2 * a * (-1));
-        result[1] = result[0];
-    } else {
-        result[0] = (-b + Math.sqrt(D)) / 2*a;
-        result[1] = (-b - Math.sqrt(D)) / 2*a; 
-    }
-    return result;
+return arr;
 }
