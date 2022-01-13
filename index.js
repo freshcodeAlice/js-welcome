85be3bc45526de0fecb5cb8b5ab779efd833aa5c
Alice <freshjs.ofte@gmail.com>
2022-01-13 20:29:35 +0200

    Spread examples

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ const concatArray = [...arrNumbers, ...arrNumbers2]; //concatination
 
 sumAllArrow(...arrNumbers); // spread
 
+ 
+const arr = [3, 2, 5, 3, 5, 2, 4, 66];
+
+const newArray = [...arr]; // пересборка массива
+
+console.log(newArray);
+
+/*
+Найдите максимальное и минимальное число, используя методы Math.max(), Math.min()
+*/
+
+const max = Math.max(...arr);
+const min = Math.min(...arr);
+console.log(max, min);
+
