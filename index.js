72ffa072b727f085c3a5cf2f7275ac093bb3a66a
Alice <freshjs.ofte@gmail.com>
2022-01-13 20:19:02 +0200

    Spread and Rest operators

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
 
-function sumAll() {
-    let res = null;
-    console.log(arguments);
-    for (let i = 0; i < arguments.length; i++) {
-       res += arguments[i];
-    }
-    return res;
-}
-
-
-const sumAllArrow = (...restArr) => {
+const sumAllArrow = (...restArr) => { //rest
+console.log(restArr);
 const sumOfIt = restArr.reduce((sum, currentValue) => sum + currentValue);
  sumOfIt;
 }
+
+const arrNumbers = [2, 4, 2, 5, 6, 8];
+const arrNumbers2 = [3, 6, 2, 4, 7, 32];
+
+const concatArray = [...arrNumbers, ...arrNumbers2]; //concatination
+
+
+
+sumAllArrow(...arrNumbers); // spread
+
