ae485c3139b9042e42134c56a3836e52cf0350bc
Alice <freshjs.ofte@gmail.com>
2022-01-13 20:39:27 +0200

    Default parameter

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 'use strict';
 
-const sumAllArrow = (...restArr) => { //rest
-console.log(restArr);
-const sumOfIt = restArr.reduce((sum, currentValue) => sum + currentValue);
- sumOfIt;
+// default parameter (параметр по умолчанию): 
+function sum (n, m = 5) {
+return n + m
 }
-
-const arrNumbers = [2, 4, 2, 5, 6, 8];
-const arrNumbers2 = [3, 6, 2, 4, 7, 32];
-
-const concatArray = [...arrNumbers, ...arrNumbers2]; //concatination
-
-
-
-sumAllArrow(...arrNumbers); // spread
-
- 
-const arr = [3, 2, 5, 3, 5, 2, 4, 66];
-
-const newArray = [...arr]; // пересборка массива
-
-console.log(newArray);
-
-/*
-Найдите максимальное и минимальное число, используя методы Math.max(), Math.min()
-*/
-
-const max = Math.max(...arr);
-const min = Math.min(...arr);
-console.log(max, min);
 
