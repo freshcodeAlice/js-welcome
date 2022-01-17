31428dd2848ee688c0debbaf5485b1d96be19cac
Alice <freshjs.ofte@gmail.com>
2022-01-17 20:47:14 +0200

    JSDOC task resolve

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 'use strict';
 
-/* 
-Найти максимальное число в массиве, который содержит числа или другие массивы чисел.
-
-const arr = [2, 3, [5, 6], 7, [8, [1, 2]], 8]
-*/
 
 /**
  * 
- * @description Рекурсивная функция нахождения максимума из массива с вложенными массивами
- * @param {Array} array Массив
+ * @description Функция, возвращает сумму двух чисел
+ * @param {number} a - Первое число
+ * @param {number} b - Второе число
  * @returns {number}
  */
 
 
-function getMax(array) {
-    let max = array[0];
-
-    for (let i = 0; i < array.length; i++) {
-        let item = array[i];
-
-        if (Array.isArray(item)) {
-            item = getMax(item);
-        }
-
-        if (item > max) {
-            max = item;
-        }
-    }
-    return max;
+function sum(a, b) {
+    return a + b;
 }
-
-
-const arr = [2, 3, [5, 6], 7, [8, [1, 2]], 8];
-
