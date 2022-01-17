680caf765d8ca8d12aed1ea9357236760010d6a6
Alice <freshjs.ofte@gmail.com>
2022-01-17 18:30:10 +0200

    Random User task resolve

--- index.js
+++ index.js
@@ -1,10 +1,81 @@
 'use strict';
 
-/* 
-Дана строка 'Я-учу-javascript!'. Замените все дефисы на '!' с помощью глобального поиска и замены
+function User(firstName, lastName, age, email, isMale, isSubscribe = false) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+    this.isMale = isMale;
+    this.isSubscribe = isSubscribe;
 
-(метод replaceAll)
+    this.getFullName = function () {
+        return `${this.firstName} ${lastName}`;
+    }
+}
+
+function createRandomUser(quantity) {
+    const res = [];
+
+    for (let i = 0; i < quantity; i++) {
+        const user = new User(
+            `Name${i}`,
+            `LastName${i}`,
+            Math.round(Math.random()*100),
+            `mail${i}@test.com`,
+            Math.random() > 0.5
+        );
+        res.push(user);
+    }
+
+    return res;
+}
+
+const userArr = createRandomUser(50);
+//console.table(userArr);
+
+
+/* Tasks:
+1. Получить массив полных имен с помощью .map()
+2. Получить массив совершеннолетних пользователей
+3. Зарандомить подписку у пользователей
+4. Получить массив подписанных совершеннолетних пользователей женского пола, но вывести только их почту
 */
 
-const string = 'Я-учу-javascript!';
-string.replaceAll('-','!');
+// Task 1
+const getFullNames = (userArray) => {
+    return userArray.map((currentUserObject) => {
+       return currentUserObject.getFullName();
+    });
+}
+
+//getFullNames(userArr);
+
+//Task 2
+
+const ADULT_AGE = 18;
+
+const getAdultUsers = (userArray) => {
+    return userArray.filter( user => user.age > ADULT_AGE);
+}
+
+//getAdultUsers(userArr);
+
+// Task 3
+
+const getRandomSubscribe = (userArray) => {
+    userArray.forEach(user => {
+        user.isSubscribe = Math.random() > 0.5;
+    });
+}
+getRandomSubscribe(userArr);
+
+//console.table(userArr);
+
+//Получить массив подписанных совершеннолетних пользователей женского пола, но вывести только их почту
+
+const getAdultSubscribedFemaleUsers = (userArray) => {
+    return userArray.filter(user => user.age > ADULT_AGE && user.isSubscribe && !user.isMale
+    ).map(user => user.email);
+}
+
+console.table(getAdultSubscribedFemaleUsers(userArr));
