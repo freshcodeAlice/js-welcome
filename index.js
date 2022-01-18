33f2cacb8cd93e5497a5ba82667ee4c383c5df65
Alice <freshjs.ofte@gmail.com>
2022-01-18 18:08:56 +0200

    Initial classes commit

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 'use strict';
 
-/**
- * 
- * @param {number} num 
- * @param {number} exp 
- * @returns {number}
- * @throws {TypeError} - в случае несовпадения типа данных
- */
-
-const pow = (num, exp) => {
-
-    if(typeof num !== 'number' || typeof exp !== 'number') {
-        throw new TypeError('Тип не тот');
-    }
-
-
-    if (exp === 1) {
-        return num;
-    }
-
-    return pow(num, exp-1)*num;
-}
 
