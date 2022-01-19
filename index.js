051c8c87579898381bf2420b2bd5cd0f70ff86f0
Alice <freshjs.ofte@gmail.com>
2022-01-19 18:34:14 +0200

    Animal Class

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict';
 
+class Animal {
+    constructor(name, amountOfLegs, type) {
+        this.name = name;
+        this.amountOfLegs = amountOfLegs;
+        this.type = type;
+    }
 
+    set name(v) {
+        if(typeof v !== 'string') {
+            throw new TypeError();
+        }
+        this._name = v;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    run() {
+        return `${this.name} is runing`;
+    }
+
+    eat() {
+        return `${this.name} is eating`;
+    }
+
+    static isAnimal(obj) {
+        return obj instanceof Animal;
+    }
+
+}
+
+
+
+const lion = new Animal('Simba', 4, 'Lion');
+const turtle = new Animal('Totilla', 4, 'Turtle');
