1a81af1c3e9c1d67f911932d05cc040f7ec1c02e
Alice <freshjs.ofte@gmail.com>
2022-01-21 21:01:14 +0200

    Date task resolve

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-/* Реализуйте класс Студент, который наследуется от класса Юзер.
-
-Этот класс должен иметь следующие свойства:
-name (имя, наследуется от User),
-lastName (фамилия, наследуется от User),
-year (год поступления)
-
-Методы:
-getFullName() - наследуется от User
-getCourse() - выводит текущий курс студента (от 1 до 5). Курс вычисляется (от текущего года отнять год поступления). Текущий год получить самостоятельно.
-
+/* Задача. Выведите на экран текущие день, месяц и год в формате 'год-месяц-день'.
 */
 
-
-
-
-class User {
-    constructor(name, lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
-
-    getFullName() {
-        return `${this.name} ${this.lastName}`;
-    }
+function getFormatDate() {
+    const now = new Date();
+    return `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
 }
 
-class Student extends User {
-    constructor(name, lastName, year) {
-        super(name, lastName);
-        this.year = year;
-    }
-
-    getCourse() {
-        const res = new Date().getFullYear() - this.year;
-
-        return res > 5 ? `${this.name} has graduated` : res;
-    }
-
-}
-
-const stud = new Student('Test', 'Tester', 2010);
+const test = getFormatDate();
