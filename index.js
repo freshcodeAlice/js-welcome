311f9a0864c92d53ece8fd38a8c2f517d589c3aa
Alice <freshjs.ofte@gmail.com>
2022-01-25 21:02:17 +0200

    Map tasks resolve

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-class Stack {
-    constructor(maxSize, ...array) {
-        this.maxSize = maxSize;
-        this.size = 0;
+'use strict'
 
-        for (const item of array) {
-            this.push(item);
-        }
+const vocabulary = new Map();
 
-    }
-
-    get isEmpty() {
-        return this._size === 0;
-    }
-
-    get size() {
-        return this._size;
-    }
-
-    get maxSize() {
-        return this._maxSize;
-    }
-
-    set maxSize(val) {
-        this._maxSize = val;
-    }
+vocabulary.set('cat', 'кошка');
+vocabulary.set('dog', 'собака');
+vocabulary.set('cucumber', 'огурец');
+vocabulary.set('tomato', 'помидор');
+vocabulary.set('orange', 'апельсин');
+vocabulary.set('pineapple', 'ананас');
+vocabulary.set('car', 'автомобиль');
+vocabulary.set('fruit', 'фрукт');
+vocabulary.set('vocabulary', 'словарь');
+vocabulary.set('can', 'банка');
+vocabulary.set('ball', 'мяч');
 
 
-    set size(val) {
-        this._size = val;
-    }
+const userInput = 'Cat dOg Tomato abracadabra cucUmber cAn balL';
 
-    push(value) {
-
-        if (this.size >= this.maxSize) {
-            throw new RangeError('Stack overflow');
-        }
-
-        this[`_${this.size}`] = value;
-        this.size = this.size + 1;
-        return this.size;
-    }
-
-    pop() {
-        if (this.isEmpty) {
-            throw new RangeError('Stack is Empty');
-        }
-
-        const lastItem = this[`_${this.size - 1}`];
-        delete this[`_${this.size - 1}`];
-        this.size = this.size - 1;
-        return lastItem;
-    }
-
-    pick() {
-        return this[`_${this.size - 1}`];
-    }
+function translate(str) {
+   return str.toLowerCase()
+   .split(' ')
+   .map((word)=> vocabulary.has(word) ? vocabulary.get(word) : word)
+   .join(' ');
 }
+
+
+/*   Task: 
+Связать пользователя и его сообщения таким образом, чтобы мы могли получить его сообщения по объекту пользователя     */
+
+const user1 = {
+    name: 'John',
+    age: 20,
+    id: 1,
+}
+
+const user2 = {
+    name: 'Jane',
+    age: 21,
+    id: 2,
+}
+
+
+const johnMsg = ['ewwre', 'rewreewre', 'rewrerew'];
+const janeMsg = ['tertwe', 'hgjghjghj', 'afdasdfsd'];
+
+const userMessageMap = new Map();
+userMessageMap.set(user1, johnMsg);
+userMessageMap.set(user2, janeMsg);
+
+/*-------*/
+
+const engArray = [...vocabulary.keys()];
+
+console.log(engArray);
+
+const ruArray = [...vocabulary.values()];
+
+console.log(ruArray);
